e77edfb707d63c932c86928cce0d9156fd717716
sodatsu01 <darumasanga.korondaa@gmail.com>
2019-11-23 00:10:51 +0900

    create till count down

--- spec/components/PomodoroTimer.spec.jsx
+++ spec/components/PomodoroTimer.spec.jsx
@@ -1,6 +1,6 @@
 /* eslint-env jest */
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import PomodoroTimer from '../../src/components/PomodoroTimer';
 
@@ -8,6 +8,14 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('Timer', () => {
   it('shows 25mins by default', () => {
-    expect(shallow(<PomodoroTimer />).text()).toBe('25:00');
+    expect(shallow(<PomodoroTimer />).find('h3').text()).toBe('25:00');
+  });
+  it('start countdown 1s by 1s when state.play=true', () => {
+    jest.useFakeTimers();
+    const wrapper = mount(<PomodoroTimer />);
+    expect(wrapper.find('h3').text()).toBe('25:00');
+    wrapper.find('button').simulate('click');
+    jest.advanceTimersByTime(1000);
+    expect(wrapper.find('h3').text()).toBe('24:59');
   });
 });
--- src/components/PomodoroTimer.jsx
+++ src/components/PomodoroTimer.jsx
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
+import StartButton from './StartButton';
+
+function convertMillisecondsToMinSec(milliseconds) {
+  const minutes = Math.floor(milliseconds / 60000);
+  const seconds = Math.floor((milliseconds - minutes * 60000) / 1000);
+  const mm = (`00${minutes}`).slice(-2);
+  const ss = (`00${seconds}`).slice(-2);
+  return `${mm}:${ss}`;
+}
+
+const WORKING_TIME = 1500000; // milliseconds
+const BREAK_TIME = 300000; // milliseconds
 
 class PomodoroTimer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      startTime: '25:00',
+      remaining: WORKING_TIME,
     };
   }
 
+  handleTick() {
+    const currentTime = Date.now();
+    const elapsed = currentTime - this.prevTime;
+    const { remaining } = this.state;
+    const nextRemaining = remaining - elapsed;
+
+    if (nextRemaining <= 0) {
+      this.setState({ remaining: BREAK_TIME });
+    } else {
+      this.prevTime = currentTime;
+      this.setState({ remaining: nextRemaining });
+    }
+  }
+
+  handleStart() {
+    this.prevTime = Date.now();
+    this.timerId = setInterval(this.handleTick.bind(this), 1000);
+  }
+
   render() {
-    const { startTime } = this.state;
+    const { remaining } = this.state;
+    const time = convertMillisecondsToMinSec(remaining);
     return (
-      <div>{startTime}</div>
+      <div>
+        <h3>{time}</h3>
+        <StartButton onClick={this.handleStart()}>test</StartButton>
+      </div>
     );
   }
 }
